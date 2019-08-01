e74bbd5e7d47d031649641006113014f34e79bab
theMatthewNewman <theMatthewNewman@gmail.com>
2019-07-31 23:10:39 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/contact'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
   root 'static_pages#home'
+  get '/contact', to: 'static_pages#contact'
+  get '/help', to: 'static_pages#help'
+  get '/abot', to: 'static_pages#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
