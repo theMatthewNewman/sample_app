e51abb50174d4054ce5833b05bc358c7d395b57e
theMatthewNewman <theMatthewNewman@gmail.com>
2019-08-06 05:22:28 -0400

    created user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(email:"test@email.com",name:"testingtesting123")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
 end
